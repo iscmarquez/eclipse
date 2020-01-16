9aa748909acefc2bac25136b4aad77b3548dfea9
eleves <eleves@PCB614-13.College_Andre-Grasset.local>
2020-01-16 16:26:53 -0500

    cast's exemples on java

---
+++ BasicTest/src/pack/Test1.java
@@ -0,0 +1,15 @@
+package pack;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a;
+		int b;
+		b = 300;
+		a = (byte) b;
+		System.out.println("a = " +a);
+		
+	}
+
+}
---
+++ BasicTest/src/pack/Test2.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//convertion double to float
+		float pi = (float)3.1416;
+		float p12 = 3.1416f; 
+	}
+
+}
