76943ec6f19c9d0c998a0b085a021ce744f11ab1
iscmarquez <iscmarquez@gmail.com>
2020-01-23 14:24:28 -0500

    correction

--- BasicTest/src/pack/Test5.java
+++ BasicTest/src/pack/Test5.java
@@ -6,23 +6,43 @@ import java.util.Scanner;
 public class Test5 {
 
 	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
+
 		Scanner sc = new Scanner(System.in);
+
 		int a = sc.nextInt();
+
 		boolean isPremier = false;
+
 		for(int i = 2; i < a; i++)
+
 		{
-			if(a%i != 0)
+
+			if(a%i == 0)
+
 			{
+
 				isPremier = true;
+
 				break;
+
 			}
+
 		}
+
 		
+
 		if(isPremier)
-			System.out.println(a + " est premier");
-		else
+
 			System.out.println(a + " n'est pas premier");
+
+		else
+
+			System.out.println(a + " est premier");
+
 	}
+
+
 
 }
